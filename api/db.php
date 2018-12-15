f6ea13ed6979323feaeec71b19cc266c215fd40a
Hussein <hdsaad01@gmail.com>
2018-12-15 13:32:37 -0500

    config changes

--- api/db.php
+++ api/db.php
@@ -2,6 +2,6 @@
 //allows for cross platform access
 header('Access-Control-Allow-Origin: *');
 //connection string to the database, ([databaseip],[user],[password],[databasename])
-$con = mysqli_connect("localhost","root","","toBuyList") or die ("could not connect database");
+$con = mysqli_connect("den1.mysql6.gear.host","tobuylist","Computer#23","tobuylist") or die ("could not connect database");
 session_start();
 ?>
